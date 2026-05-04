96e2e577064811326660b23709bf47c16bcc2c54
The Pirate Game Studio <zolius.rm@gmail.com>
2026-05-04 08:31:09 +0300

    Update bot

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "👻MADE WITH ZOLIUS & ARAYSH👻",
+        name: "👻MADE BY ZOLIUS & ARAYSH👻",
         // Activity type number (0 = Playing).
         type: 3, 
       },
